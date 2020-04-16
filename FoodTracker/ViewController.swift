6170250f0a21b0077b38e02ffbe26e8640c904c2
JamesDansie <jdansie8336@gmail.com>
2020-04-15 20:00:06 -0700

    first commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by James Dansie on 4/15/20.
+//  Copyright © 2020 James Dansie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
