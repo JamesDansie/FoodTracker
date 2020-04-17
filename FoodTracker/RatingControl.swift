6f3a48e2f571a1bf8413f459594af789ef1f8fca
JamesDansie <jdansie8336@gmail.com>
2020-04-17 13:11:46 -0700

    star ratings work now

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,110 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by James Dansie on 4/17/20.
+//  Copyright © 2020 James Dansie. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RatingControl: UIStackView {
+    
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0){
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton){
+        print("Button pressed")
+        guard let index = ratingButtons.firstIndex(of: button) else {
+            fatalError("The button, \(button), is not inthe ratingsButton array: \(ratingButtons)")
+        }
+        
+        let selectedRatings = index + 1
+        
+        if selectedRatings == rating {
+            // if the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // otherwise set the rating to the selected star
+            rating = selectedRatings
+        }
+    }
+
+    //MARK: Private Methods
+    private func setupButtons(){
+        
+        // clear out old buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        // Load button images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
+        for _ in 0..<starCount {
+            // Create a button
+            let button = UIButton()
+            
+            // Set button images
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
+            
+            // Constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the buttonto the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to the array
+            ratingButtons.append(button)
+        }
+        
+        updateButtonSelectionStates()
+    }
+    
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of the button is less than the rating, it should be selected (to show as filled)
+            button.isSelected = index < rating
+        }
+    }
+}
